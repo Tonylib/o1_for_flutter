0b0ee058f7124a77fa105d621d151543a8b4c989
Anthony <anthony.libby@wildforkfoods.com>
2024-05-18 13:01:35 -0400

    O1

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'O1',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
